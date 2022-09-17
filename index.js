2ed15de57614daaad1c9bea3b66e557bff269b2b
Daniel <zodexnl@gmail.com>
2022-09-17 19:00:17 +0200

    changed to discord.js v14 (latest)

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits } = require('discord.js')
+require('dotenv/config')
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+
+client.on('ready', () => {
+    console.log('The bot is ready')
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping') {
+        message.reply({
+            content: 'pong',
+        })
+    }
+})
+
+client.login(process.env.TOKEN)
