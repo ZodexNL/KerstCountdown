6db3203b0f0ae1f0ba2e6fce668a802df0b3c612
Daniel <zodexnl@gmail.com>
2022-09-17 21:00:58 +0200

    now triggers when kerst is in message

--- index.js
+++ index.js
@@ -15,32 +15,64 @@ client.on('ready', () => {
     console.log('The bot is ready')
 })
 
+const words = ["kerst"] 
+
+
+
 client.on('messageCreate', (message) => {
-    if (message.content === 'kerst') {
+    if (message.content.includes ('kerst')) {
 
-        const date = new Date('12/25/2022')
-        let now = new Date()
 
-        let difference = date.getTime() - now.getTime()
+        for (var i = 0; i < words.length; i++) {
+            if (message.content.includes(words[i])) {
+                message.reply('ja')
+              break;
+            }
+          }
 
-        let totaldays = Math.ceil(difference / (1000 * 60 * 60 * 24))
+        // const date = new Date('12/25/2022')
+        // const date2 = new Date('12/27/2022')
+        
+        // let now = new Date()
 
-        console.log(totaldays)
-        // let now = Date(now)
-        // let strNow = now.toString()
+        // let difference = date.getTime() - date2.getTime()
 
-        // let diffTime = Math.abs(now - date)
-        // let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+        // let totaldays = Math.ceil(difference / (1000 * 60 * 60 * 24))
 
-        // let timeElapsed = Date.now()
-        // let today = new Date(timeElapsed)
-        // let ding = today.toLocaleDateString()
+        // console.log(totaldays)
 
-        // console.log(ding)
 
-        message.reply({
-            content: '${totaldays}',
-        })
+        // switch (true){
+        //     case totaldays == 0:
+        //         message.reply(`Het is vandaag kerst! Fijne dagen toegewenst`)
+        //         break;
+
+        //     case totaldays == 1:
+        //         message.reply(`Morgen is het kerst! Heb je alles in huis?`)
+        //         break;
+
+        //     case totaldays > 1:
+        //         message.reply(`Over ${totaldays} dagen is het kerst!`)
+        //         break;
+        //     case totaldays == -1:
+        //         message.reply(`Het is vandaag 2e kerstdag! Weer gourmetten zeker?`)
+        //         break;
+        //     case totaldays ==-2:
+        //         message.reply(`Restjes eten en uitbuiken....`)
+        //         break;
+        // }
+
+
+        // const date = new Date('12/25/2022')
+        // let now = new Date()
+
+        // let difference = date.getTime() - now.getTime()
+
+        // let totaldays = Math.ceil(difference / (1000 * 60 * 60 * 24))
+
+        // console.log(totaldays)
+
+        // message.reply(`Over ${totaldays} dagen is het kerst!`)
     }
 })
 
