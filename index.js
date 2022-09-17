3127eb7bc0ef9abae08e3e0303866d0efd2bb6b7
Daniel <zodexnl@gmail.com>
2022-09-17 21:45:41 +0200

    added emoji in variable to shorten code

--- index.js
+++ index.js
@@ -14,11 +14,14 @@ const client = new Client({
 client.on('ready', () => {
     console.log('The bot is ready')
 })
+
+const date = new Date('12/25/2022')
+const emoji = ':regional_indicator_k: :regional_indicator_e: :regional_indicator_r: :regional_indicator_s: :regional_indicator_t:'
         
 client.on('messageCreate', (message) => {
     if (message.content.includes ('kerst')) {
 
-        const date = new Date('12/25/2022')
+
         let now = new Date()
 
         let difference = date.getTime() - now.getTime()
@@ -28,18 +31,18 @@ client.on('messageCreate', (message) => {
 
         switch (true){
             case totaldays == 0:
-                message.reply(`Het is vandaag :regional_indicator_k: :regional_indicator_e: :regional_indicator_r: :regional_indicator_s: :regional_indicator_t:! Fijne dagen toegewenst`)
+                message.reply(`Het is vandaag ${emoji}! Fijne dagen toegewenst`)
                 break;
 
             case totaldays == 1:
-                message.reply(`Morgen is het :regional_indicator_k: :regional_indicator_e: :regional_indicator_r: :regional_indicator_s: :regional_indicator_t:! Heb je alles in huis?`)
+                message.reply(`Morgen is het ${emoji}! Heb je alles in huis?`)
                 break;
 
             case totaldays > 1:
-                message.reply(`Over ${totaldays} dagen is het :regional_indicator_k: :regional_indicator_e: :regional_indicator_r: :regional_indicator_s: :regional_indicator_t:!`)
+                message.reply(`Over ${totaldays} dagen is het ${emoji}!`)
                 break;
             case totaldays == -1:
-                message.reply(`Het is vandaag 2e :regional_indicator_k: :regional_indicator_e: :regional_indicator_r: :regional_indicator_s: :regional_indicator_t: dag! Weer gourmetten zeker?`)
+                message.reply(`Het is vandaag 2e ${emoji} dag! Weer gourmetten zeker?`)
                 break;
             case totaldays == -2:
                 message.reply(`Restjes eten en uitbuiken....`)
