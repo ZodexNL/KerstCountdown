e0c60ae3cd8e6dc1fc2d3c8dc81cd2ff0935302c
Daniel <zodexnl@gmail.com>
2022-09-17 20:12:22 +0200

    calculates the diff in days

--- index.js
+++ index.js
@@ -16,9 +16,30 @@ client.on('ready', () => {
 })
 
 client.on('messageCreate', (message) => {
-    if (message.content === 'ping') {
+    if (message.content === 'kerst') {
+
+        const date = new Date('12/25/2022')
+        let now = new Date()
+
+        let difference = date.getTime() - now.getTime()
+
+        let totaldays = Math.ceil(difference / (1000 * 60 * 60 * 24))
+
+        console.log(totaldays)
+        // let now = Date(now)
+        // let strNow = now.toString()
+
+        // let diffTime = Math.abs(now - date)
+        // let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+
+        // let timeElapsed = Date.now()
+        // let today = new Date(timeElapsed)
+        // let ding = today.toLocaleDateString()
+
+        // console.log(ding)
+
         message.reply({
-            content: 'pong',
+            content: '${totaldays}',
         })
     }
 })
