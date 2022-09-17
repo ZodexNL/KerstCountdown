77af70667ab78e948dbb53b2bd0f8a9e28a2f18f
Daniel <zodexnl@gmail.com>
2022-09-17 21:40:47 +0200

    changed to emoji text

--- index.js
+++ index.js
@@ -14,65 +14,37 @@ const client = new Client({
 client.on('ready', () => {
     console.log('The bot is ready')
 })
-
-const words = ["kerst"] 
-
-
-
+        
 client.on('messageCreate', (message) => {
     if (message.content.includes ('kerst')) {
 
+        const date = new Date('12/25/2022')
+        let now = new Date()
 
-        for (var i = 0; i < words.length; i++) {
-            if (message.content.includes(words[i])) {
-                message.reply('ja')
-              break;
-            }
-          }
+        let difference = date.getTime() - now.getTime()
+        let totaldays = Math.ceil(difference / (1000 * 60 * 60 * 24))
 
-        // const date = new Date('12/25/2022')
-        // const date2 = new Date('12/27/2022')
-        
-        // let now = new Date()
+        console.log(totaldays)
 
-        // let difference = date.getTime() - date2.getTime()
+        switch (true){
+            case totaldays == 0:
+                message.reply(`Het is vandaag :regional_indicator_k: :regional_indicator_e: :regional_indicator_r: :regional_indicator_s: :regional_indicator_t:! Fijne dagen toegewenst`)
+                break;
 
-        // let totaldays = Math.ceil(difference / (1000 * 60 * 60 * 24))
+            case totaldays == 1:
+                message.reply(`Morgen is het :regional_indicator_k: :regional_indicator_e: :regional_indicator_r: :regional_indicator_s: :regional_indicator_t:! Heb je alles in huis?`)
+                break;
 
-        // console.log(totaldays)
-
-
-        // switch (true){
-        //     case totaldays == 0:
-        //         message.reply(`Het is vandaag kerst! Fijne dagen toegewenst`)
-        //         break;
-
-        //     case totaldays == 1:
-        //         message.reply(`Morgen is het kerst! Heb je alles in huis?`)
-        //         break;
-
-        //     case totaldays > 1:
-        //         message.reply(`Over ${totaldays} dagen is het kerst!`)
-        //         break;
-        //     case totaldays == -1:
-        //         message.reply(`Het is vandaag 2e kerstdag! Weer gourmetten zeker?`)
-        //         break;
-        //     case totaldays ==-2:
-        //         message.reply(`Restjes eten en uitbuiken....`)
-        //         break;
-        // }
-
-
-        // const date = new Date('12/25/2022')
-        // let now = new Date()
-
-        // let difference = date.getTime() - now.getTime()
-
-        // let totaldays = Math.ceil(difference / (1000 * 60 * 60 * 24))
-
-        // console.log(totaldays)
-
-        // message.reply(`Over ${totaldays} dagen is het kerst!`)
+            case totaldays > 1:
+                message.reply(`Over ${totaldays} dagen is het :regional_indicator_k: :regional_indicator_e: :regional_indicator_r: :regional_indicator_s: :regional_indicator_t:!`)
+                break;
+            case totaldays == -1:
+                message.reply(`Het is vandaag 2e :regional_indicator_k: :regional_indicator_e: :regional_indicator_r: :regional_indicator_s: :regional_indicator_t: dag! Weer gourmetten zeker?`)
+                break;
+            case totaldays == -2:
+                message.reply(`Restjes eten en uitbuiken....`)
+                break;
+        }
     }
 })
 
